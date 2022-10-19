3d1c1140a8e8ba79e6ef34e65f1b3ecdaaf263f3
shekharkausalye <64542143+shekharkausalye@users.noreply.github.com>
2022-10-19 15:25:21 +0530

    Added if loop for odd even number

--- first.py
+++ first.py
@@ -1,5 +1,9 @@
 name = input("Enter your name: ")
 print("Welcome to Python")
 print(f"Hello {name}.")
-print('Bye.')
-print('See you later.')
+
+a = int(input("Enter integer number: "))
+if a%2 == 0:
+    print('Entered number is Even')
+else:
+    print('Entered number is Odd')
